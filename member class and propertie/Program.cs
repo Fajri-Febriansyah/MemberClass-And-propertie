0307df1c7e5deb7495d21e977fbbaddbcbc0423a
Fajri-Febriansyah <febriansyahfajri7@gmail.com>
2023-05-22 14:54:04 +0700

    initial init

---
+++ member class and propertie/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace member_class_and_propertie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            planetiket tiket1 = new planetiket();
+            planetiket tiket2 = new planetiket();
+
+            tiket1.origin = "YOGYAKARTA";
+            tiket1.destination = "PKU";
+            tiket1.cost = 100;
+            tiket1.currency = "USD";
+
+            tiket2.origin = "JAKARTA";
+            tiket2.destination = "PKU";
+            tiket2.cost = 750000;
+            tiket2.currency = "USD";
+
+            tiket1.printsummary();
+            Console.WriteLine();
+            tiket2.printsummary();
+        }
+    }
+}
---
+++ member class and propertie/planetiket.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace member_class_and_propertie
+{
+    internal class planetiket
+    {
+        public string origin { get; set; }
+        public string destination { get; set; }
+        public int cost { get; set; }
+        public string currency { get; set; }
+
+        public void printsummary() 
+        {
+            Console.WriteLine("================================================");
+            Console.WriteLine("ORIGIN                    =    {0}", origin);
+            Console.WriteLine("DESTINATION               =    {0}", destination);
+            Console.WriteLine("COST                      =    {0}", cost);
+
+            if (currency == "USD")
+            {
+            Console.WriteLine("CONVERT TO RUPIAH         =    {0}", cost*15000);  
+            }
+            else if (currency == "IDR")  
+            {
+            Console.WriteLine("CONVERT TO DOLLAR         =    {0}", cost/15000);   
+            }
+            Console.WriteLine("================================================");
+
+        }
+    }
+}
